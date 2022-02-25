823e61cd49e9b900dcb43766a2f3b176f63aeace
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-26 00:44:13 +0700

    tes

--- anibot/utils/helper.py
+++ anibot/utils/helper.py
@@ -331,14 +331,13 @@ def get_auth_btns(media, user, data, name, lsqry: str = None, lspage: int = None
     else:
         btn.append(InlineKeyboardButton(text="➕ Ke Fav" if data[3] is not True else "➖ dari Fav", callback_data=f"fav_{media}_{data[0]}{qry}{pg}_{user}"))
         if media=="ANIME":
-            btn.append(InlineKeyboardButton("View More", f"https://t.me/ccgnimeX_bot/?start=request_{qry}",
-                        "url",
-                    ))
             btn.append(InlineKeyboardButton("Download", switch_inline_query_current_chat=name))
         btn.append(InlineKeyboardButton(
             text="Add to List" if data[1] is False else "Update List",
             callback_data=f"lsadd_{media}_{data[0]}{qry}{pg}_{user}" if data[1] is False else f"lsupdt_{media}_{data[0]}_{data[2]}{qry}{pg}_{user}"
             ))
+        btn.append(InlineKeyboardButton("View More", f"https://t.me/ccgnimeX_bot/?start=anime_{qry}",
+                        "url",))
                 
     return btn
 
