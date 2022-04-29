7e750de99ce8c794d7328c284c267769d4113431
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-04-29 21:01:10 +0700

    tes

--- anibot/plugins/livechartme.py
+++ anibot/plugins/livechartme.py
@@ -121,7 +121,7 @@ async def livechart_parser():
             listlinks = ""
             for ii in ls[i]:
                 listlinks += '\n__'+ii[0]+'__: [Link]('+ii[1]+')'
-            msgssp.append(['**Update Anime Terbaru di Subsplease**\n\n' + i + listlinks, 'https://nyaa.si/?q='+re.sub(r' ', '%20', re.sub(r'(\().*?(\))', r'', i).strip())])
+            msgssp.append(['**Anime Update dari Subplease**\n\n' + i + listlinks, 'https://nyaa.si/?q='+re.sub(r' ', '%20', re.sub(r'(\().*?(\))', r'', i).strip())])
 ##########################
 
 
