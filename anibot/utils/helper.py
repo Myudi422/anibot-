69018bccad40889c8652b8d214ab0313e83201f6
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-02 21:41:28 +0700

    tess

--- anibot/utils/helper.py
+++ anibot/utils/helper.py
@@ -286,7 +286,7 @@ def get_btns(media, user: int, result: list, lsqry: str = None, lspage: int = No
         if result[0] == 6:
             buttons.append([InlineKeyboardButton(str(day_(result[0]-1)), callback_data=f"sched_{result[0]-1}_{user}")])
     if media == "MANGA" and sfw == "False":
-        buttons.append([InlineKeyboardButton("More Info", url=result[1][2])])
+        buttons.append([InlineKeyboardButton("More Info", url=result[1][2])], [InlineKeyboardButton(text="⌧ Hapus", callback_data=f'neko_delete, {user}')])
     if media == "AIRING" and sfw == "False":
         buttons.append([InlineKeyboardButton("More Info", url=result[1][0])], [InlineKeyboardButton(text="⌧ Hapus", callback_data=f'neko_delete, {user}')])
     name = None
