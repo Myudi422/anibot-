320aaa9a51bbf04beab60c3ed586f281b232f44d
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-05-04 16:50:21 +0700

    tes

--- anibot/utils/helper.py
+++ anibot/utils/helper.py
@@ -299,7 +299,10 @@ def get_btns(media, user: int, result: list, lsqry: str = None, lspage: int = No
         # Tombol Download Guest.
     else:
         if media=="ANIME" and sfw == "False":
-            buttons.append([InlineKeyboardButton("Unduh", switch_inline_query_current_chat=name)])
+            btnlist = []
+            if cq.message.chat.type == "private":
+                btnlist.append(InlineKeyboardButton("Unduh", switch_inline_query_current_chat=name))
+            button.append(btnlist) 
             
     if len(result)>3:
         if result[3] == "None":
@@ -337,7 +340,10 @@ def get_auth_btns(media, user, data, name, lsqry: str = None, lspage: int = None
     else:
         btn.append(InlineKeyboardButton(text="⨭ Ke Fav" if data[3] is not True else "⨪ dari Fav", callback_data=f"fav_{media}_{data[0]}{qry}{pg}_{user}"))
         if media=="ANIME":
-            btn.append(InlineKeyboardButton("Unduh", switch_inline_query_current_chat=name))
+            btnlist = []
+            if cq.message.chat.type == "private":
+                btnlist.append(InlineKeyboardButton("Unduh", switch_inline_query_current_chat=name))
+            button.append(btnlist) 
             
         btn.append(InlineKeyboardButton(
             text="⨮ Ke List" if data[1] is False else "Ganti List",
